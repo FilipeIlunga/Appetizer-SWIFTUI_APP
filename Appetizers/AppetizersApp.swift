2dea3e7aa92f097312f97dbd9079f45c12125d2e
Filipe Ilunga Xindanhi <fxindanhi@nelogica.com.br>
2022-04-16 01:27:50 -0300

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by DEV on 16/04/22.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
