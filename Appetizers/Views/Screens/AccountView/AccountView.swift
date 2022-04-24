aa001b01f4a38c62b98cec0686a318eb1d4debed
Filipe Ilunga Xindanhi <fxindanhi@nelogica.com.br>
2022-04-23 23:52:29 -0300

    Add AccountView infos

--- Appetizers/Views/Screens/AccountView/AccountView.swift
+++ Appetizers/Views/Screens/AccountView/AccountView.swift
@@ -8,11 +8,51 @@
 import SwiftUI
 
 struct AccountView: View {
+    @State var extraNapkins: Bool = true
+    @State var frequentRefills: Bool = false
+    @State var firstName: String = ""
+    @State var lastName: String = ""
+    @State var email: String = ""
+    @State var birthday: Date = Date()
+
     var body: some View {
         NavigationView {
-            Text("Account")
-                .navigationTitle("Account")
-        }    }
+            Form {
+                Section(header: Text("PERSONAL INFO")) {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
+                    
+                    DatePicker("Birthday", selection: $birthday,displayedComponents: .date)
+                        .pickerStyle(.segmented)
+                    
+                    Button {
+                        print("Tapped")
+                    } label: {
+                        Text("Save Changes")
+                    }
+                        
+                }
+                
+                Section(header: Text("REQUESTS")) {
+                    APToggle(isOn: $extraNapkins, title: "Extra Napkins")
+                    APToggle(isOn: $frequentRefills, title: "Frequent Refills")
+                }
+            }.navigationTitle("Account")
+        }
+    }
+}
+
+struct APToggle: View {
+    @Binding var isOn: Bool
+    let title: String
+    var body: some View {
+        Toggle(title,isOn: $isOn)
+            .toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
+    }
 }
 
 struct AccountView_Previews: PreviewProvider {
