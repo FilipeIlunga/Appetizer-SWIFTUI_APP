25ac3e98c6bef6955be8f6a23db9a2af30e009f6
Filipe Ilunga Xindanhi <fxindanhi@nelogica.com.br>
2022-04-24 01:51:42 -0300

    Created orderView

--- Appetizers/Model/Appetizer.swift
+++ Appetizers/Model/Appetizer.swift
@@ -23,7 +23,7 @@ struct AppetizerResponse: Decodable {
 }
 
 struct MockData {
-    static let sampleApptezier = Appetizer(id: 0001,
+    static let sampleApptezier = Appetizer(id: 0000,
                                            name: "Test Appetizer",
                                            description: "This is the description for my appetizer. It's yummy",
                                            price: 9.97,
@@ -32,5 +32,32 @@ struct MockData {
                                            protein: 99,
                                            carbs: 99)
     
-    static let appetizer = [sampleApptezier,sampleApptezier,sampleApptezier,sampleApptezier]
+    static let orderItemOne = Appetizer(id: 0001,
+                                           name: "Order Item one",
+                                           description: "This is the description for my appetizer. It's yummy",
+                                           price: 9.97,
+                                           imageURL: "",
+                                           calories: 99,
+                                           protein: 99,
+                                           carbs: 99)
+    
+    static let orderItemTwo = Appetizer(id: 0002,
+                                           name: "Order Item two",
+                                           description: "This is the description for my appetizer. It's yummy",
+                                           price: 9.97,
+                                           imageURL: "",
+                                           calories: 99,
+                                           protein: 99,
+                                           carbs: 99)
+    
+    static let orderItemThree = Appetizer(id: 0003,
+                                           name: "Order Item three",
+                                           description: "This is the description for my appetizer. It's yummy",
+                                           price: 9.97,
+                                           imageURL: "",
+                                           calories: 99,
+                                           protein: 99,
+                                           carbs: 99)
+    
+    static let orderItems = [orderItemOne,orderItemTwo,orderItemThree]
 }
--- Appetizers/Views/Screens/OrderView/OrderView.swift
+++ Appetizers/Views/Screens/OrderView/OrderView.swift
@@ -8,12 +8,36 @@
 import SwiftUI
 
 struct OrderView: View {
+    @State private var orderItems = MockData.orderItems
+    
     var body: some View {
         NavigationView {
-            Text("Order")
-                .navigationTitle("Order")
+            VStack{
+                List {
+                    ForEach(orderItems) { appetizer in
+                        AppetizerListCell(appetizer: appetizer)
+                    }
+                    .onDelete { indexSet in
+                        orderItems.remove(atOffsets: indexSet)
+                    }
+                }
+                .listStyle(.insetGrouped)
+                
+                Button {
+                    print("Order placed")
+                } label: {
+                    APButton(title: "$99.99 - Place Order")
+                }
+                .padding(.bottom, 25)
+            }
+            .navigationTitle("Order")
         }
     }
+    
+    func deleteItem(at offsets: IndexSet) {
+        orderItems.remove(atOffsets: offsets)
+    }
+    
 }
 
 struct OrderView_Previews: PreviewProvider {
